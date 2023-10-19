245ba5757fb0ec27cda98506a6525d15c86e7248
K-Pomian <pomian.konrad@gmail.com>
2023-10-19 10:55:56 +0200

    Init

---
+++ src/main/java/pl/pomian/booksdistribution/BooksDistributionApplication.java
@@ -0,0 +1,13 @@
+package pl.pomian.booksdistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksDistributionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksDistributionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pomian/booksdistribution/BooksDistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pomian.booksdistribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksDistributionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
